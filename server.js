e68ef62897371986c6257206a0fb1821a7f8e6dd
DENNY <albionkuonline600@gmail.com>
2024-07-29 15:26:24 +0700

    feat:fix session after sleep mode

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const { Server } = require('socket.io');
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,42 +13,69 @@ const io = new Server(server);
 let isAuthenticated = false;
 const contacts = new Set();
 
-const client = new Client({
-    authStrategy: new LocalAuth()
-});
+let client = createClient();
 
-client.on('qr', (qr) => {
-    qrcode.toDataURL(qr, (err, url) => {
-        if (err) {
-            console.error('Failed to generate QR code', err);
-        } else {
-            io.emit('qr', url);
-        }
+function createClient() {
+     const client = new Client({
+        authStrategy: new LocalAuth(),
+        puppeteer: {
+            headless: true,
+            // args: [ '--no-sandbox', '--disable-gpu', ],
+            pipe: true
+        },
+        webVersionCache: { type: 'remote', remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html', }
     });
-});
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-    isAuthenticated = true;
-    io.emit('ready');
-});
+    client.on('qr', (qr) => {
+        qrcode.toDataURL(qr, (err, url) => {
+            if (err) {
+                console.error('Failed to generate QR code', err);
+            } else {
+                io.emit('qr', url);
+            }
+        });
+    });
 
-client.on('authenticated', () => {
-    console.log('Client is authenticated!');
-});
+    client.on('ready', () => {
+        console.log('Client is ready!');
+        isAuthenticated = true;
+        io.emit('ready');
+    });
 
-client.on('auth_failure', msg => {
-    console.error('AUTHENTICATION FAILURE', msg);
-    isAuthenticated = false;
-});
+    client.on('authenticated', () => {
+        console.log('Client is authenticated!');
+    });
 
-client.on('message', msg => {
-    console.log('Message received:', msg.body);
-    contacts.add(msg.from);
-    io.emit('message', msg);
-});
+    client.on('auth_failure', msg => {
+        console.error('AUTHENTICATION FAILURE', msg);
+        isAuthenticated = false;
+        restartClient();
+    });
 
-client.initialize();
+    client.on('disconnected', reason => {
+        console.error('Client was logged out', reason);
+        isAuthenticated = false;
+        restartClient();
+    });
+
+    client.on('message', msg => {
+        console.log('Message received:', msg.body);
+        contacts.add(msg.from);
+        io.emit('message', msg);
+    });
+
+    client.initialize();
+
+    return client;
+}
+
+function restartClient() {
+    console.log('Restarting client...');
+    if (client) {
+        client.destroy();
+    }
+    client = createClient();
+}
 
 io.on('connection', (socket) => {
     console.log('a user connected');
@@ -56,7 +85,10 @@ io.on('connection', (socket) => {
         if (isAuthenticated) {
             number = formatNumber(number);
             console.log(`Sending message to ${number}`);
-            client.sendMessage(number, message).catch(err => console.error('Error sending message:', err));
+            client.sendMessage(number, message).catch(err => {
+                console.error('Error sending message:', err);
+                restartClient();
+            });
         }
     });
 
@@ -65,21 +97,23 @@ io.on('connection', (socket) => {
             number = formatNumber(number);
             console.log(`Sending file to ${number}`);
             const media = MessageMedia.fromFilePath(filePath);
-            client.sendMessage(number, media).catch(err => console.error('Error sending file:', err));
+            client.sendMessage(number, media).catch(err => {
+                console.error('Error sending file:', err);
+                restartClient();
+            });
         }
     });
 });
 
 function formatNumber(number) {
-    // Pastikan nomor telepon diformat ke bentuk internasional
-    number = number.replace(/\D/g, ''); // Hapus semua karakter non-digit
+    number = number.replace(/\D/g, ''); 
     if (number.startsWith('0')) {
-        number = '62' + number.slice(1); // Ganti '0' di depan dengan kode negara '62' untuk Indonesia
+        number = '62' + number.slice(1); 
     }
     if (!number.startsWith('62')) {
-        number = '62' + number; // Tambahkan '62' di depan jika belum ada
+        number = '62' + number;
     }
-    return number + '@c.us'; // Tambahkan domain yang diharapkan oleh WhatsApp
+    return number + '@c.us'; 
 }
 
 app.get('/', (req, res) => {
